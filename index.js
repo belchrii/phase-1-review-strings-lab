fc1f69665aae9ae6058538a5229ced727714b19f
chris bell <elmntwzard@gmail.com>
2023-03-28 12:52:08 -0500

    Adds my file.md

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser = 'Chris Bell';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const upperCaseCurrentUser = currentUser.toUpperCase();
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`;
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
